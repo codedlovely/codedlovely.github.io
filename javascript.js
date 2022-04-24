6180e655386834feac993af0aeb6b475ed904244
codedlovely <submerginglife@gmail.com>
2022-04-24 18:48:24 +0900

    Create home page

---
+++ javascript.js
@@ -0,0 +1,6 @@
+
+// output footer copyright
+let currentYear = new Date().getFullYear();
+const divCopyright = document.querySelector('.copyright');
+divCopyright.textContent += currentYear + ' CodedLovely';
+
