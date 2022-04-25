836a625090ac9c0eea6f8654346888db046fb334
codedlovely <submerginglife@gmail.com>
2022-04-25 15:05:18 +0900

    Add responsive menu

--- javascript.js
+++ javascript.js
@@ -4,3 +4,21 @@ let currentYear = new Date().getFullYear();
 const divCopyright = document.querySelector('.copyright');
 divCopyright.textContent = '\u{0000A9} ' + currentYear + ' CodedLovely';
 
+// display hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+// close hamburger menu
+const navLink = document.querySelectorAll(".navilink");
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
