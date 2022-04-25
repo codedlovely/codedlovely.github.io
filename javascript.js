568ea5ea6ac402bb9a642bd4a3bd068bbee36c00
codedlovely <submerginglife@gmail.com>
2022-04-25 10:48:44 +0900

    Modify layout and add new images

--- javascript.js
+++ javascript.js
@@ -2,5 +2,5 @@
 // output footer copyright
 let currentYear = new Date().getFullYear();
 const divCopyright = document.querySelector('.copyright');
-divCopyright.textContent = '\u{0000A9} ' + currentYear + ' Coded\u{002665}Lovely';
+divCopyright.textContent = '\u{0000A9} ' + currentYear + ' CodedLovely';
 
