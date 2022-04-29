69b6d01b2985fa2e51c2c2cf1af99ab11b73182f
codedlovely <submerginglife@gmail.com>
2022-04-29 14:26:01 +0900

    Add scroll-back-to-top button

--- javascript.js
+++ javascript.js
@@ -26,3 +26,18 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// Scroll back to top
+mybutton = document.getElementById("scrollBack");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
