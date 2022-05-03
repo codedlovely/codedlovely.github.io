018417f311fefdd7f7695472cd11b31e1b304b9e
codedlovely <submerginglife@gmail.com>
2022-05-03 22:20:21 +0900

    Adjust layout

--- javascript.js
+++ javascript.js
@@ -1,31 +1,11 @@
-
 // output footer copyright
 let copyrightYear = 2022;
 let currentYear = new Date().getFullYear();
 if (currentYear > copyrightYear) {
     copyrightYear += `-${currentYear}`;
 }
-const divCopyright = document.querySelector('.copyright');
+const divCopyright = document.getElementById('copyright')
 divCopyright.textContent = '\u{0000A9} ' + copyrightYear + ' CodedLovely';
-
-// display hamburger menu
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-// close hamburger menu
-const navLink = document.querySelectorAll(".navilink");
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
 
 // Scroll back to top
 mybutton = document.getElementById("scrollBack");
