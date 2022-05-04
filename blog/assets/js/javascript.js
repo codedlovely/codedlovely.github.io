699adbb0eba6e3f4221f34db62d66e8d1a9914d1
codedlovely <submerginglife@gmail.com>
2022-05-04 15:32:10 +0900

    Add blog pages

---
+++ blog/assets/js/javascript.js
@@ -0,0 +1,8 @@
+// output footer copyright
+let copyrightYear = 2022;
+let currentYear = new Date().getFullYear();
+if (currentYear > copyrightYear) {
+    copyrightYear += `-${currentYear}`;
+}
+const divCopyright = document.getElementById('copyright')
+divCopyright.textContent = '\u{0000A9} ' + copyrightYear + ' CodedLovely';
